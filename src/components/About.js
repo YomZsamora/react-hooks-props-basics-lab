084b0300932adf8b7c703d2dcc7826a80780b50d
yomZsamora <samora.y@adzumi.co.ke>
2022-03-10 19:45:37 +0300

    Done fetching props data from parent component inside the About component.

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function ifBioEmpty(bio) {
+   if(bio) {
+      return <p>{bio}</p>
+   } 
 }
 
-export default About;
+function About(props) {
+   return (
+      <div id="about">
+      <h2>About Me</h2>
+      {ifBioEmpty(props.bio)}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {/* add your <Links /> component here */}
+      </div>
+      );
+   }
+   
+   export default About;
+   
